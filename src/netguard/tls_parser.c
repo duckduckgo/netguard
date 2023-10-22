0307b57b894301339e6670001f17eb79a4c789df
Aitor Viana <aitorvs@gmail.com>
2023-10-22 17:31:04 +0100

    Remove some index checks

--- src/netguard/tls_parser.c
+++ src/netguard/tls_parser.c
@@ -11,7 +11,7 @@
 #include "platform.h"
 #include "tls.h"
 
-static int parse_tls_server_name(const uint8_t *data, size_t data_len, char *server_name);
+static int parse_tls_server_name(const uint8_t *data, const size_t data_len, char *server_name);
 static int parse_extensions(const uint8_t*, size_t, char *);
 static int parse_server_name_extension(const uint8_t*, size_t, char *);
 
@@ -36,7 +36,7 @@ static int parse_server_name_extension(const uint8_t*, size_t, char *);
  *  -3  invalid TLS client hello
  *  -4  invalid TLs packet
  */
-static int parse_tls_server_name(const uint8_t *data, size_t data_len, char *server_name) {
+static int parse_tls_server_name(const uint8_t *data, const size_t data_len, char *server_name) {
     *server_name = 0;
 
     if (data_len < TLS_HEADER_LEN) {
@@ -69,8 +69,9 @@ static int parse_tls_server_name(const uint8_t *data, size_t data_len, char *ser
     }
 
     /* TLS record length */
-    size_t len = ((size_t)data[3] << 8) + (size_t)data[4] + TLS_HEADER_LEN;
-    data_len = MIN(len, data_len);
+//    uint16_t len = ((size_t)data[3] << 8) + (size_t)data[4] + TLS_HEADER_LEN;
+    size_t len = ntohs(*((uint16_t *) (data + 3))) + TLS_HEADER_LEN;
+//    data_len = MIN(len, data_len);
     if (data_len < len) {
         // purposely don't return as we have checks later on
         log_print(PLATFORM_LOG_PRIORITY_WARN, "TLS data length smaller than expected, proceed anyways");
@@ -78,9 +79,9 @@ static int parse_tls_server_name(const uint8_t *data, size_t data_len, char *ser
 
     /* handshake */
     size_t pos = TLS_HEADER_LEN;
-    if (pos + 1 > data_len) {
-        return -5;
-    }
+//    if (pos + 1 > data_len) {
+//        return -5;
+//    }
 
     if (data[pos] != 0x1) {
         // not a client hello
@@ -97,17 +98,18 @@ static int parse_tls_server_name(const uint8_t *data, size_t data_len, char *ser
     pos += 38;
 
     // Session ID
-    if (pos + 1 > data_len) return -7;
+//    if (pos + 1 > data_len) return -7;
     len = (size_t)data[pos];
     pos += 1 + len;
 
     /* Cipher Suites */
-    if (pos + 2 > data_len) return -8;
-    len = ((size_t)data[pos] << 8) + (size_t)data[pos + 1];
+//    if (pos + 2 > data_len) return -8;
+//    len = ((size_t)data[pos] << 8) + (size_t)data[pos + 1];
+    len = ntohs(*((uint16_t *) (data + pos)));
     pos += 2 + len;
 
     /* Compression Methods */
-    if (pos + 1 > data_len) return -9;
+//    if (pos + 1 > data_len) return -9;
     len = (size_t)data[pos];
     pos += 1 + len;
 
@@ -117,15 +119,16 @@ static int parse_tls_server_name(const uint8_t *data, size_t data_len, char *ser
     }
 
     /* Extensions */
-    if (pos + 2 > data_len) {
-        return -11;
-    }
-    len = ((size_t)data[pos] << 8) + (size_t)data[pos + 1];
+//    if (pos + 2 > data_len) {
+//        return -11;
+//    }
+//    len = ((size_t)data[pos] << 8) + (size_t)data[pos + 1];
+    len = ntohs(*((uint16_t *) (data + pos)));
     pos += 2;
 
-    if (pos + len > data_len) {
-        return -12;
-    }
+//    if (pos + len > data_len) {
+//        return -12;
+//    }
     return parse_extensions(data + pos, len, server_name);
 }
 
@@ -136,15 +139,15 @@ static int parse_extensions(const uint8_t *data, size_t data_len, char *hostname
     /* Parse each 4 bytes for the extension header */
     while (pos + 4 <= data_len) {
         /* Extension Length */
-        len = ((size_t)data[pos + 2] << 8) +
-              (size_t)data[pos + 3];
+//        len = ((size_t)data[pos + 2] << 8) +(size_t)data[pos + 3];
+        len = ntohs(*((uint16_t *) (data + pos + 2)));
 
         /* Check if it's a server name extension */
         if (data[pos] == 0x00 && data[pos + 1] == 0x00) {
             /* There can be only one extension of each type, so we break
                our state and move p to beinnging of the extension here */
-            if (pos + 4 + len > data_len)
-                return -20;
+//            if (pos + 4 + len > data_len)
+//                return -20;
             return parse_server_name_extension(data + pos + 4, len, hostname);
         }
         pos += 4 + len; /* Advance to the next extension header */
@@ -161,12 +164,12 @@ static int parse_server_name_extension(const uint8_t *data, size_t data_len, cha
     size_t len;
 
     while (pos + 3 < data_len) {
-        len = ((size_t)data[pos + 1] << 8) +
-              (size_t)data[pos + 2];
+//        len = ((size_t)data[pos + 1] << 8) + (size_t)data[pos + 2];
+        len = ntohs(*((uint16_t *) (data + pos + 1)));
 
-        if (pos + 3 + len > data_len) {
-            return -30;
-        }
+//        if (pos + 3 + len > data_len) {
+//            return -30;
+//        }
 
         switch (data[pos]) { /* name type */
             case 0x00: /* host_name */
